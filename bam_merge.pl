22f20ee48da122a494f3a68225d028407d970770
Mike Covington <mfcovington@gmail.com>
2012-05-30 23:06:55 -0700

    bam_merge.pl - initial clean-up, but noticed that script may have been sent incomplete

--- bam_merge.pl
+++ bam_merge.pl
@@ -1,18 +1,27 @@
+#!/usr/bin/env perl
+# bam_merge.pl
+# Upendra Devisetty & Mike Covington
+# created: 2012-05-30
+#
+# Description: 
+#
 use strict;
 use warnings;
 use File::Basename;
+use autodie;
 
 my @matched_files = `ls $ARGV[0]/*fq.bam`;
 my $file_out;
 
 for my $file_matched (@matched_files) {
-        (my $prefix, my $path) = fileparse($file_matched, "_matched.fq.bam");
-        my $file_unmatched_1 = $path . $prefix . "_1_unmatched.bam";
-        my $file_unmatched_2 = $path . $prefix . "_2_unmatched.bam";
-        my $file_out = $path . $prefix . ".merged.bam";
-        `samtools merge $file_out $file_matched $file_unmatched_1 $file_unmatched_2`;
+    ( my $prefix, my $path ) = fileparse( $file_matched, "_matched.fq.bam" );
+    my $file_unmatched_1 = $path . $prefix . "_1_unmatched.bam";
+    my $file_unmatched_2 = $path . $prefix . "_2_unmatched.bam";
+    my $file_out         = $path . $prefix . ".merged.bam";
+    `samtools merge $file_out $file_matched $file_unmatched_1 $file_unmatched_2`;
 }
 
-open OUT1, ">$file_out" or die "\n\nCannot open output $file_out\n";
 
-close (OUT1)
+open OUT1, ">", $file_out;
+
+close OUT1;
